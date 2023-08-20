16ffb9ce4c69e0c46e4180e81b0fba9c70583952
Jacob Sanders <sanders.jms@gmail.com>
2023-08-20 11:31:48 -0600

    added initialize test for board

---
+++ spec/board_spec.rb
@@ -0,0 +1,10 @@
+describe Board do
+  
+  context 'when creating an instance of board' do
+    
+    it 'instantiates with the correct default board' do
+      board = Board.new
+      expect(Board.positions).to eq((1..42).to_a)
+    end
+  end
+end
