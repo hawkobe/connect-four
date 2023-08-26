26600e4e59ef271d2d25cd6be80af2c006cac698
hawkobe <sanders.jms@gmail.com>
2023-08-26 00:10:40 -0600

    created a board_full method and refactored board to instantiate with nil

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,7 @@ class Board
   attr_accessor :positions
 
   def initialize
-    @positions = Array.new(6) { Array.new(7, "-") }
+    @positions = Array.new(6) { Array.new(7, nil) }
   end
 
   def display
@@ -13,7 +13,7 @@ class Board
       print "\n"
       print "   | "
       row.each do |space|
-        print space + " | "
+        print "#{space}  | "
       end
       print "\n"
     end
@@ -22,5 +22,9 @@ class Board
     print "\n"
     puts "   | 1 | 2 | 3 | 4 | 5 | 6 | 7 |"
     2.times { print "\n" }
+  end
+
+  def board_full?
+    @positions.none? { |row| row.include?(nil) }
   end
 end
