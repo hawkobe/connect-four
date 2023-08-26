917385a5b423f035a0f0271057e262ecf87c05a1
hawkobe <sanders.jms@gmail.com>
2023-08-25 21:09:18 -0600

    created display method for board

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,25 @@ class Board
   attr_accessor :positions
 
   def initialize
-    @positions = (1..42).to_a
+    @positions = Array.new(6) { Array.new(7, "-") }
   end
 
+  def display
+    print "\n"
+    @positions.each do |row|
+      print "   "
+      29.times { print "-" }
+      print "\n"
+      print "   | "
+      row.each do |space|
+        print space + " | "
+      end
+      print "\n"
+    end
+    print "   "
+    29.times { print "=" }
+    print "\n"
+    puts "   | 1 | 2 | 3 | 4 | 5 | 6 | 7 |"
+    2.times { print "\n" }
+  end
 end
