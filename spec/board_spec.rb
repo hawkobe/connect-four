8d77c1c576b8c26658f6dbed50ff653b08ef3a9d
hawkobe <sanders.jms@gmail.com>
2023-08-26 00:11:24 -0600

    started tests for board_full method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,9 +7,34 @@ describe Board do
   context 'when creating an instance of board' do
     
     it 'instantiates with the correct default board' do
-      board = Board.new
       board_positions = board.positions
-      expect(board_positions).to eq((1..42).to_a)
+      expect(board_positions).to eq(Array.new(6) {Array.new(7, nil)})
+    end
+  end
+
+  describe '#board_full?' do
+
+    context 'when the board is full' do
+
+      before do
+        board.instance_variable_set(
+          :@positions, 
+          board.positions.map do |row|
+          row.map { |position| position = 'x' }
+        end)
+      end 
+
+      it 'returns true' do
+        expect(board.board_full?).to be(true)
+      end
+
+    end
+
+    context 'when the board is not full' do
+
+      it 'returns false' do
+        expect(board.board_full?).to be(false)
+      end
     end
   end
 end
