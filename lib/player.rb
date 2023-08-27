9760d49ec1203376a2155f3fb54ab42081dfa2f6
hawkobe <sanders.jms@gmail.com>
2023-08-27 03:12:12 -0600

    added symbol to player class initialize method

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,13 @@
 class Player
 
-  attr_reader :name
+  attr_reader :name, :symbol
 
-  def initialize(name)
+  def initialize(name, symbol)
     @name = name
+    @symbol = symbol
   end
-
 end
+
+jacob = Player.new("Jacob", "\u26AB")
+
+puts jacob.symbol
