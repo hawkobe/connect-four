7631cf5faadae5bb3b73f11de708b58a99735ba8
hawkobe <sanders.jms@gmail.com>
2023-09-22 09:24:05 -0600

    added regex for player choice

--- lib/player.rb
+++ lib/player.rb
@@ -9,6 +9,12 @@ class Player
 
   def select_column
     puts "Please select a column in which you would like to drop your piece"
+    regex = /^[1-7]$/
     selected_column = gets.chomp 
-  end  
+    until regex.match? selected_column
+      puts "#{selected_column} is an invalid input (Please select a number 1-7)"
+      selected_column = gets.chomp
+    end
+    selected_column.to_i
+  end
 end
