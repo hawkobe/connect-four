15b6c910e689742102eaa9e1d702cf8878168277
Jacob Sanders <sanders.jms@gmail.com>
2023-10-29 11:50:21 -0600

    finished all tests for player setup method

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,3 +1,39 @@
+require_relative '../lib/connect_four.rb'
+require_relative '../lib/board.rb'
+
 describe ConnectFour do
 
+  subject(:game) { described_class.new }
+
+  describe '#player_setup' do
+
+    context 'when setting up players' do
+      
+      before do
+        allow(game).to receive(:puts)
+        allow(game).to receive(:gets).and_return("Jacob", "Crystal")
+        game.player_setup
+      end
+
+      it 'sets up a player class for player 1 with the correct name' do
+        player_one_name = game.instance_variable_get(:@player_one).name
+        expect(player_one_name).to eq("Jacob")
+      end
+
+      it 'sets up the correct symbol for player 1' do
+        player_one_symb = game.instance_variable_get(:@player_one).symbol
+        expect(player_one_symb).to eq("\u25CF")
+      end
+
+      it 'sets up player class for player 2 with the correct name' do
+        player_two_name = game.instance_variable_get(:@player_two).name
+        expect(player_two_name).to eq("Crystal")
+      end
+
+      it 'sets up the correct symbol for player 2' do
+        player_two_symb = game.instance_variable_get(:@player_two).symbol
+        expect(player_two_symb).to eq("\u25CB")
+      end
+    end
+  end
 end
