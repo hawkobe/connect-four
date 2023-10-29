fd84e6e4b4ed3362930a25e34555713b07c41704
Jacob Sanders <sanders.jms@gmail.com>
2023-10-29 11:50:02 -0600

    added player setup method

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,11 +1,29 @@
+require_relative 'board.rb'
+require_relative 'player.rb'
+
 class ConnectFour
+  attr_reader :player1
 
   def initialize
     @board = Board.new
+    @current_player = nil
   end 
 
-  def play
+  def player_setup
+    puts "Player 1, please enter your name"
+    player_one_name = gets.chomp
+    @player_one = Player.new(player_one_name, "\u25CF")
+    puts "Thanks, #{@player_one.name}! Your symbol is #{@player_one.symbol}"
+
+    puts "Player 2, please enter your name"
+    player_two_name = gets.chomp
+    @player_two = Player.new(player_two_name, "\u25CB")
+    puts "Thanks, #{@player_two.name}! Your symbol is #{@player_two.symbol}"
   end
 end
 
-# study private mehtods again to figure out how to not have to use attr_accessor
+game = ConnectFour.new
+game.player_setup
+
+
+# study private methods again to figure out how to not have to use attr_accessor
