a3bbc20df74af89af2de1b4ccf378cca460c656a
Jacob Sanders <sanders.jms@gmail.com>
2023-11-05 11:40:42 -0700

    finished tests for when column has positions for execute move

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -41,6 +41,33 @@ describe ConnectFour do
     # needs to take #column_full from board class with select_column as arg
     # and then execute if position available
     # board.column_full?(@current_player.select_column - 1)
+    before do
+      game.instance_variable_set(:@current_player, Player.new("Jacob", "\u25CB"))
+    end
+
+    context 'when the column has open positions' do
+
+      it 'puts the token in the correct column' do
+        player_selection = 1
+        expect { game.execute_move(player_selection) }.to change { game.board.positions[0] }
+      end
+      
+      it 'puts the symbol in the correct position when column is empty' do
+        player_selection = 1
+        current_player_symbol = game.current_player.symbol
+        game.execute_move(player_selection)
+        expect(game.board.positions[0][0]).to eq(current_player_symbol)
+      end
+
+      it 'puts the symbol in the correct position when column has some symbols already' do
+        player_selection = 1
+        current_player_symbol = game.current_player.symbol
+        opposing_player_symbol = "\u25CF"
+        game.board.positions[0] = [opposing_player_symbol, opposing_player_symbol, '-', '-', '-', '-', '-']
+        game.execute_move(player_selection)
+        expect(game.board.positions[0][2]).to eq(current_player_symbol)
+      end
+    end
   end
 
   describe '#game_over?' do
