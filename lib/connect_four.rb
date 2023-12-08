41f4d0c3ea2d155fdfb7613b93e75b45c3cf51b4
Jacob Sanders <sanders.jms@gmail.com>
2023-12-07 17:35:24 -0700

    finished game loop, draw message, win message, switch player methods

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -10,30 +10,55 @@ class ConnectFour
   end 
 
   def player_setup
-    puts "Player 1, please enter your name"
+    puts "Player 1, please enter your name".white
     player_one_name = gets.chomp
-    @player_one = Player.new(player_one_name, "\u25CF")
-    puts "Thanks, #{@player_one.name}! Your symbol is #{@player_one.symbol}"
-
-    puts "Player 2, please enter your name"
+    @player_one = Player.new(player_one_name, "\u25CF".red)
+    puts "Thanks, #{@player_one.name}! Your symbol is #{@player_one.symbol}".white
+    print "\n"
+    puts "Player 2, please enter your name".white
     player_two_name = gets.chomp
-    @player_two = Player.new(player_two_name, "\u25CB")
-    puts "Thanks, #{@player_two.name}! Your symbol is #{@player_two.symbol}"
+    @player_two = Player.new(player_two_name, "\u25CB".yellow)
+    puts "Thanks, #{@player_two.name}! Your symbol is #{@player_two.symbol}".white
+    print "\n"
+
+    @current_player = [@player_one, @player_two].sample
   end
 
-  # def play
-  #   until game_over?
-  #   end
-  # end
+  def game_loop
+    loop do
+      switch_player
+      puts "Alright #{@current_player.name}, it's your turn"
+      puts "This is what your board looks like"
+      board.display
+      break winner_message if game_won?(execute_move(board.select_column), @current_player.symbol)
+      break draw_message if @board.board_full?
+      print "\e[2J\e[f"
+    end
+  end
 
-  def game_over?(position_array, symbol)
+  def winner_message
+    puts "#{@current_player.name} has won the game!"
+    puts "Here's what your final board looked like"
+    board.display
+  end
+
+  def draw_message
+    puts "Wow, it looks like it was a draw!"
+    puts "Here's your final board"
+    board.display
+  end
+
+  def game_won?(position_array, symbol)
     veritcal_win?(position_array, symbol) || 
-    horizontal_win?(position_array, symbol) || 
+    horizontal_win?(position_array, symbol) ||
     diag_down_left_win?(position_array, symbol) || 
     diag_down_right_win?(position_array, symbol) || 
     diag_up_left_win?(position_array, symbol) || 
-    diag_up_right_win?(position_array, symbol) ||
-    board.board_full?
+    diag_up_right_win?(position_array, symbol)
+  end
+
+  def switch_player
+    @current_player == @player_one ? @current_player = @player_two : @current_player = @player_one
   end
 
   def execute_move(column_number)
@@ -158,10 +183,11 @@ class ConnectFour
   end
 end
 
-# game = ConnectFour.new
+game = ConnectFour.new
 # game.instance_variable_set(:@current_player, Player.new("Jacob", "\u25CF"))
 
-# game.execute_move(1)
-# game.board.display
+game.player_setup
+
+game.game_loop
 
 # study private methods again to figure out how to not have to use attr_accessor
